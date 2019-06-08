e827e8f90ef4ccd8d0fcba55a8d7f5352a12129a
svk192 <sabaviqar@gmail.com>
2019-06-08 16:57:28 -0400

    basic javascript and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+
+var wordArray = ['Friends','Suits','TheWire','GameOfThrones','TheOffice','BreakingBad']; 
+
+var currentWord;
+var blanks = [];
+var userChoice;
+var userChoices = [];
+var wins = 0;
+var losses = 0;
+var currentWordArray = [];
+var remainingLetters; 
+var guesses;
+var currentWordText;
+
+// var currentWordText= document.getElementById("currentWord");
+var guessesText= document.getElementById("guesses");
+var winsText = document.getElementById("Wins");
+var lossesText = document.getElementById("losses");
+
+function newGame(){
+    document.getElementById("currentWord").innerHTML ='';
+    currentWord =  wordArray[Math.floor(Math.random()*wordArray.length)];
+    remainingLetters = currentWord.length;
+    userChoices = [];    
+    // blanks = [];
+    currentWordArray = currentWord.split("");
+        for(i = 0; i<currentWord.length;i++){
+        // blanks.push("_");
+        currentWordText = document.createElement("div");
+        currentWordText.innerHTML = "_";
+        currentWordText.classList.add("letter")
+        currentWordText.id = i;
+        document.getElementById("currentWord").appendChild(currentWordText);
+        }
+    guesses = 12;
+
+    guessesText.textContent = "Guesses: " + guesses;
+    winsText.textContent = "Wins: "+ wins;
+    lossesText.textContent="Losses: "+losses;
+}
+
+newGame();
+
+    document.onkeyup = function(event) {
+    userChoice = event.key;
+        if(userChoices.indexOf(userChoice.toLowerCase()) === -1){         
+            
+            if(currentWordArray.indexOf(userChoice.toLowerCase()) === -1
+            && currentWordArray.indexOf(userChoice.toUpperCase()) === -1 ){
+                guesses--;    userChoices.push(userChoice);
+                if (guesses===0){losses++;newGame();}
+                }
+            else{            
+                    for(i=0; i <currentWord.length; i++){
+                        if(userChoice.toLowerCase() === currentWordArray[i].toLowerCase() )
+                        {
+                            remainingLetters--;
+                            document.getElementById(i).textContent =currentWordArray[i];
+                            userChoices.push(userChoice);
+
+                        }
+                    }
+                    if (remainingLetters===0) {wins++;newGame();}
+                    
+                }
+
+            guessesText.textContent = "Guesses: " + guesses;
+            winsText.textContent = "Wins: "+ wins;
+            lossesText.textContent="Losses: "+losses;
+        }
+        else{
+        alert("already pressed that!")
+        }       
+    }
+// }
